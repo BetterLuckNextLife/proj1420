3f86a964c4544d265f6d47d467a5848454c9c91b
_void <fejkfejkov25@gmail.com>
2025-03-16 12:30:00 +0300

    Add gitignore

--- theme/imageGenerator.py
+++ theme/imageGenerator.py
@@ -1,20 +1,17 @@
 from PIL import Image
 import numpy as np
 import sys
-from pprint import pp
 from typing import List
 sys.path.append("../mainapp/")
 import caller
 
-def remap(l: List[int]) -> List[int]:
-    return list(map(lambda x: (x,0,0), l))
 
-
-def gen_image():
+def gen_image(modules: bool = False, stations: bool = False):
     arr = caller.getTestMap()
-    data = np.zeros((64, 64, 3), dtype=np.uint8)
-    for i in range(64):
-        for j in range(64):
+    larr = len(arr)
+    data = np.zeros((larr, larr, 3), dtype=np.uint8)
+    for i in range(larr):
+        for j in range(larr):
             data[i, j] = [arr[i][j], 0, 0]
     img = Image.fromarray(data)
     img.save("templates/map.png")
