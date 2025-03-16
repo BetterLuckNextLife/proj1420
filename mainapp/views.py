a3a3ce456d7ac514f2924da51a14b6c1eb285f07
BetterLuckNextLife <rykovegor020@gmail.com>
2025-03-16 14:12:43 +0300

    Динамическое обновление сайта

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import imageGenerator
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+    if request.method == "POST":
+        if "linker" in request.POST:
+            addr = request.POST.get("linker")
+            imageGenerator.gen_image(addr=addr)
+            return redirect('/')
