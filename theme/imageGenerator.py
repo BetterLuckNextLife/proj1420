c97d8b94ce3e0ab315a007568535157edf4279d9
_void <fejkfejkov25@gmail.com>
2025-03-16 12:52:25 +0300

    Update image generation.

--- theme/imageGenerator.py
+++ theme/imageGenerator.py
@@ -6,14 +6,22 @@ sys.path.append("../mainapp/")
 import caller
 
 
-def gen_image(modules: bool = False, stations: bool = False):
-    arr = caller.getTestMap()
-    larr = len(arr)
+def gen_image(modules: bool = False, stations: bool = False, coords: bool = False) -> None:
+    '''gen_image(modules: bool = False, stations: bool = False, coords: bool = False)
+        example: gen_image(True)
+    '''
+    arr = caller.getFullMap()
+    larr = 256
     data = np.zeros((larr, larr, 3), dtype=np.uint8)
-    for i in range(larr):
-        for j in range(larr):
-            data[i, j] = [arr[i][j], 0, 0]
+    for tile in range(4):
+        for tile2 in range(4):
+            for i in range(64):
+                for j in range(64):
+                    data[tile*64+i, tile2*64+j] = [arr[tile2+tile][i][j], 0, 0]
     img = Image.fromarray(data)
     img.save("templates/map.png")
     img.show()
-gen_image()
+
+
+if __name__ == "__main__":
+    gen_image()
