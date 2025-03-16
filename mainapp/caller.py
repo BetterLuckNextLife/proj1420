50a1f043cd988160359491973763a2f1b547b6dd
BetterLuckNextLife <rykovegor020@gmail.com>
2025-03-16 11:51:36 +0300

    Добавлены функции для получения данных

--- mainapp/caller.py
+++ mainapp/caller.py
@@ -1,7 +1,22 @@
 import requests
 import json
 
-def getTestData() -> list:
-    r = requests.get("https://olimp.miet.ru/ppo_it/api")
+testaddr = "https://olimp.miet.ru/ppo_it"
+
+def getTestMap():
+    r = requests.get(testaddr + "/api")
     data = json.loads(r.text)
-    print(data["message"]["data"])
+    tile = data["message"]["data"]
+    return tile
+
+
+def getTestCoordsAndPrice() -> list:
+    r = requests.get(testaddr + "/coords")
+    data = json.loads(r.text)
+    listener = data["message"]["listener"]
+    sender = data["message"]["sender"]
+    price = data["message"]["price"]
+    return listener, sender, price
+
+print(getTestCoordsAndPrice())
+print(getTestMap())
