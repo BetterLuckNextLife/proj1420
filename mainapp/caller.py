8eeb14844ab4511acdb1113a964c99a9afeb6f78
BetterLuckNextLife <rykovegor020@gmail.com>
2025-03-16 12:25:15 +0300

    Добавлена функция получения всех тайлов

--- mainapp/caller.py
+++ mainapp/caller.py
@@ -3,17 +3,30 @@ import json
 
 testaddr = "https://olimp.miet.ru/ppo_it"
 
-def getTestMap(addr=testaddr):
+def getMap(addr=testaddr):
     r = requests.get(f"{addr}/api")
     data = json.loads(r.text)
     tile = data["message"]["data"]
     return tile
 
 
-def getTestCoordsAndPrice(addr=testaddr) -> list:
+def getCoordsAndPrice(addr=testaddr) -> list:
     r = requests.get(f"{addr}/api/coords")
     data = json.loads(r.text)
     listener = data["message"]["listener"]
     sender = data["message"]["sender"]
     price = data["message"]["price"]
     return listener, sender, price
+
+def getFullMap(addr=testaddr) -> list:
+    alltiles = []
+    hashable = set()
+    while len(alltiles) < 16:
+        r = requests.get(f"{addr}/api")
+        data = json.loads(r.text)
+        tile = data["message"]["data"]
+        tileString = ""
+        for i in tile:
+            tileString += "".join(str(i))
+        if tileString not in hashable: hashable.add(tileString); alltiles.append(tile)
+    return alltiles
